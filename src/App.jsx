7e4991d5caf857afba3e7a74cb7412b0828162df
nedaashkan <117584245+nedaashkan@users.noreply.github.com>
2023-11-20 16:42:54 +0330

    added Tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <div className="App">
+      <h1 className="text-4xl">hi</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
